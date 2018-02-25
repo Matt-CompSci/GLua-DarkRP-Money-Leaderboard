510879f13d1f4f9981274220b31a8ecfc85aa7ad
Unknown <gmatt1923@gmail.com>
2018-02-25 19:17:23 +0000

    Added Files

---
+++ lua/entities/money_leaderboard/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Money Leaderboard"
+ENT.Author = "MaTT"
+ENT.Category = "StarlightRP - Misc"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
