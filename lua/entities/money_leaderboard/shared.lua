68fdd0113af5a53c7fdce14bfb281980688582cf
Matt1923 <gmatt1923@gmail.com>
2018-03-12 18:14:02 +0000

    Update shared.lua

--- lua/entities/money_leaderboard/shared.lua
+++ lua/entities/money_leaderboard/shared.lua
@@ -2,6 +2,5 @@ ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 ENT.PrintName = "Money Leaderboard"
 ENT.Author = "MaTT"
-ENT.Category = "StarlightRP - Misc"
 ENT.Spawnable = true
 ENT.AdminSpawnable = false
